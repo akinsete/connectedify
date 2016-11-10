da35d159a0e5ba3e5e4a7339e03756afe5825066
hurlatunde <hurlatunde@gmail.com>
2016-11-10 09:17:23 +0100

    first comment

---
+++ app/src/main/java/com/projetct/olatundeowokoniran/con/connectedify/Landing.java
@@ -0,0 +1,17 @@
+package com.projetct.olatundeowokoniran.con.connectedify;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by olatundeowokoniran on 11/9/16.
+ */
+
+public class Landing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
